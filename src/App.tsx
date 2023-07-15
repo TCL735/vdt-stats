40258c965d909ec1cbe0bf102d699ca0346c1bf2
Timmy Luong <TCL735@users.noreply.github.com>
2023-07-14 19:29:42 -0700

    feat: mvp of chart (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,150 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {Box, createStyles} from '@mantine/core'
+import dayjs from 'dayjs'
+import './App.css'
+import {EChartsOption, ReactECharts} from './react-echarts'
 
-function App() {
+const useStyles = createStyles(() => ({
+  datapointTooltip: {
+    color: 'black',
+    margin: '-1rem',
+    maxWidth: '18rem',
+    padding: '0.75rem',
+    textAlign: 'left',
+    textShadow: '1px 1px rgba(0,0,0,0.15)',
+    userSelect: 'none',
+    whiteSpace: 'pre-wrap',
+    wordBreak: 'break-all',
+  },
+  negativeCurrency: {
+    color: 'red',
+  },
+  positiveCurrency: {
+    color: 'black',
+  },
+}))
+
+const currency = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+})
+
+const ARIA = 'Aria'
+const BELLAGIO = 'Bellagio'
+const EL_CORTEZ = 'El Cortez'
+const FLAMINGO = 'Flamingo'
+const MANDALAY_BAY = 'Mandalay Bay'
+const PALAZZO = 'Palazzo'
+
+const dayTrips = [
+  ['2023-01-11', 10095.0, BELLAGIO],
+  ['2023-01-18', 585.0, MANDALAY_BAY],
+  ['2023-01-25', -9100.0, PALAZZO],
+
+  ['2023-02-08', 8225, MANDALAY_BAY],
+  ['2023-02-14', -11700.0, ARIA],
+  ['2023-02-15', -2180.0, FLAMINGO],
+  ['2023-02-21', -13400.0, PALAZZO],
+
+  ['2023-03-01', -1560.0, PALAZZO],
+  ['2023-03-15', 5100.0, ARIA],
+  ['2023-03-22', -4900.0, PALAZZO],
+  ['2023-03-29', 5100.0, BELLAGIO],
+
+  ['2023-04-05', -945.0, EL_CORTEZ],
+  ['2023-04-12', -2000.0, PALAZZO],
+  ['2023-04-13', -25000.0, PALAZZO],
+
+  ['2023-05-10', 6701.0, BELLAGIO],
+  ['2023-05-17', -14500.0, ARIA],
+  ['2023-05-24', -13700.0, PALAZZO],
+
+  ['2023-06-01', -7340.0, PALAZZO],
+  ['2023-06-07', 3200.0, ARIA],
+  ['2023-06-14', 1215.0, BELLAGIO],
+  ['2023-06-21', 13010.0, ARIA],
+  ['2023-06-27', -4570.0, PALAZZO],
+
+  ['2023-07-05', 31700.0, PALAZZO],
+  ['2023-07-12', -7400.0, ARIA],
+]
+
+export const App = () => {
+  const {classes} = useStyles()
+  const option: EChartsOption = {
+    axisPointer: {
+      triggerTooltip: true,
+    },
+    tooltip: {
+      trigger: 'axis',
+      show: true,
+      axisPointer: {
+        type: 'cross',
+      },
+      formatter: (params: any) => {
+        const dataIndex = params[0].dataIndex
+        let content = `<div class="${classes.datapointTooltip}">`
+        if (Array.isArray(params) && params.length) {
+          content += `<b>Date: ${dayjs(params[0].data[0]).format(
+            'MMM DD, YYYY'
+          )}</b><br/><span class="${
+            Number(dayTrips[dataIndex][1]) < 0
+              ? classes.negativeCurrency
+              : classes.positiveCurrency
+          }">Win/Loss: ${currency.format(
+            Number(dayTrips[dataIndex][1])
+          )}</span><br/><b class="${
+            params[0].data[1] < 0
+              ? classes.negativeCurrency
+              : classes.positiveCurrency
+          }">YTD: ${currency.format(params[0].data[1])}</b><br/><b>Location: ${
+            params[0].data[2]
+          }</b>`
+        }
+        content += '</div>'
+        return content
+      },
+    },
+    xAxis: {
+      type: 'category',
+      axisLabel: {
+        rotate: 45,
+        formatter: (date: string) => dayjs(date).format('MMM DD'),
+      },
+    },
+    yAxis: {
+      type: 'value',
+      name: 'USD $',
+      nameLocation: 'middle',
+      nameGap: 100,
+      axisLabel: {
+        formatter: (money: number) => currency.format(money),
+      },
+    },
+    series: [
+      {
+        type: 'line',
+        data: dayTrips.reduce((acc, dayTrip, index) => {
+          if (index > 0) {
+            acc.push([
+              dayTrip[0],
+              Number(acc[index - 1][1]) + Number(dayTrip[1]),
+              dayTrip[2],
+            ])
+          } else {
+            acc.push(dayTrip)
+          }
+          return acc
+        }, [] as typeof dayTrips),
+      },
+    ],
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box h={600} mt={100}>
+        <ReactECharts option={option} renderer="canvas" />
+      </Box>
     </div>
-  );
+  )
 }
-
-export default App;
---
+++ src/react-echarts.tsx
@@ -0,0 +1,194 @@
+import {useElementSize, useMergedRef} from '@mantine/hooks'
+import {
+  BarChart,
+  BarSeriesOption,
+  LineChart,
+  LineSeriesOption,
+  CustomChart,
+  CustomSeriesOption,
+  ScatterChart,
+  ScatterSeriesOption,
+} from 'echarts/charts'
+import {
+  DataZoomInsideComponent,
+  DataZoomSliderComponent,
+  DatasetComponent,
+  DatasetComponentOption,
+  GridComponent,
+  GridComponentOption,
+  LegendComponent,
+  LegendComponentOption,
+  ToolboxComponent,
+  TooltipComponent,
+  MarkAreaComponent,
+  TooltipComponentOption,
+  TransformComponent,
+  DataZoomComponentOption,
+  MarkPointComponent,
+} from 'echarts/components'
+import type {ComposeOption, ECharts, SetOptionOpts} from 'echarts/core'
+import {getInstanceByDom, init, registerLayout, use} from 'echarts/core'
+import {LabelLayout} from 'echarts/features'
+import {CanvasRenderer, SVGRenderer} from 'echarts/renderers'
+import {CSSProperties, forwardRef, useEffect} from 'react'
+import {StageHandlerOverallReset} from 'echarts/types/src/util/types'
+
+export const graphColours = [
+  '#50C9F0',
+  '#4C5994',
+  '#43B4BA',
+  '#9F58AD',
+  '#EC8F46',
+  '#474341',
+  '#2663A9',
+  '#D14F97',
+  '#6AA146',
+  '#D7E16F',
+] as const
+
+// register only the necessary ECharts components
+use([
+  BarChart,
+  CanvasRenderer,
+  SVGRenderer,
+  DatasetComponent,
+  GridComponent,
+  LabelLayout,
+  CustomChart,
+  LegendComponent,
+  LineChart,
+  ScatterChart,
+  TooltipComponent,
+  MarkAreaComponent,
+  MarkPointComponent,
+  ToolboxComponent,
+  TransformComponent,
+  DataZoomInsideComponent,
+  DataZoomSliderComponent,
+])
+
+export type EChartsOption = ComposeOption<
+  | BarSeriesOption
+  | GridComponentOption
+  | DatasetComponentOption
+  | DataZoomComponentOption
+  | LegendComponentOption
+  | LineSeriesOption
+  | ScatterSeriesOption
+  | TooltipComponentOption
+  | CustomSeriesOption
+>
+
+const getDefaultChartOption = (theme: 'light' | 'dark'): EChartsOption => ({
+  color: theme === 'light' ? graphColours : undefined,
+  textStyle: {
+    fontFamily:
+      '-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji',
+    color: '#303a42',
+  },
+})
+
+export interface ReactEChartsProps {
+  option: EChartsOption
+  style?: CSSProperties
+  settings?: SetOptionOpts
+  loading?: boolean
+  theme?: 'light' | 'dark'
+  renderer?: 'svg' | 'canvas'
+  // This does not provide a lot of type safety... But works for now :)
+  // TODO: Figure out a way to provide type safety here
+  chartEvents?: Record<string, Function>
+  onChartReady?: (chart: ECharts) => void
+  customLayoutFunction?: StageHandlerOverallReset
+  className?: string
+}
+
+export const ReactECharts = forwardRef<HTMLDivElement, ReactEChartsProps>(
+  (
+    {
+      option: chartOption,
+      style,
+      settings,
+      loading,
+      theme = 'light',
+      renderer = 'canvas',
+      onChartReady,
+      chartEvents,
+      className,
+      customLayoutFunction,
+      ...rest
+    },
+    ref
+  ) => {
+    const {ref: container, width, height} = useElementSize<HTMLDivElement>()
+    const mergedRef = useMergedRef(container, ref)
+
+    useEffect(() => {
+      // Initialize chart
+      let chart: ECharts | undefined
+      if (container.current !== null) {
+        if (customLayoutFunction) {
+          registerLayout(customLayoutFunction as any)
+        }
+        chart = init(container.current, theme, {renderer})
+        onChartReady?.(chart)
+      }
+
+      // Return cleanup function
+      return () => {
+        chart?.dispose()
+      }
+    }, [container, customLayoutFunction, onChartReady, renderer, theme])
+
+    useEffect(() => {
+      // Chart resize
+      if (container.current !== null) {
+        const chart = getInstanceByDom(container.current)
+        chart?.resize({animation: {duration: 300}})
+      }
+    }, [width, height, container])
+
+    useEffect(() => {
+      // Update chart
+      if (container.current !== null) {
+        // augment the option with the default configuration
+        const option = {...getDefaultChartOption(theme), ...chartOption}
+        const chart = getInstanceByDom(container.current)
+        chart?.setOption(option, settings)
+      }
+    }, [chartOption, container, settings, theme])
+
+    useEffect(() => {
+      // change in loading state
+      if (container.current !== null) {
+        const chart = getInstanceByDom(container.current)
+        loading ? chart?.showLoading() : chart?.hideLoading()
+      }
+    }, [container, loading, theme])
+
+    useEffect(() => {
+      // Bind events
+      if (container.current !== null) {
+        const chart = getInstanceByDom(container.current)
+        if (chart && chartEvents) {
+          Object.keys(chartEvents).forEach((eventName) => {
+            if (chartEvents[eventName]) {
+              chart?.on(eventName, (param) =>
+                chartEvents[eventName]?.(param, chart)
+              )
+            }
+          })
+        }
+      }
+    }, [chartEvents, container])
+
+    return (
+      <div
+        ref={mergedRef}
+        style={{width: '100%', height: '100%', ...style}}
+        className={className}
+        {...rest}
+      />
+    )
+  }
+)
