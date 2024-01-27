efb7f919832a42bca6837ed4f41fad3541cbd458
Timmy Luong <TCL735@users.noreply.github.com>
2024-01-26 16:09:50 -0800

    feat: day trip 4 (#43)

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ export const App = () => {
           <YearlyStats
             dayTrips={dayTrips2024}
             label="Vegas Daytripper's 2024 Win/Loss"
-            lineColor="green"
+            lineColor="orange"
             year={activeTab}
           />
         </Tabs.Panel>
