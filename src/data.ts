bdfd97a8648f39953371881b57d1a683a514dfba
Timmy Luong <TCL735@users.noreply.github.com>
2024-07-08 11:02:09 -0700

    feat: day trip 32

--- src/data.ts
+++ src/data.ts
@@ -202,4 +202,5 @@ export const dayTrips2024: Array<DayTrip> = [
   ],
   [dayjs('2024-06-27').valueOf(), [-205], [BELLAGIO]],
   [dayjs('2024-07-02').valueOf(), [-8200, -2950], [MANDALAY_BAY, PALAZZO]],
+  [dayjs('2024-07-05').valueOf(), [-7000, -600], [VENETIAN, BELLAGIO]],
 ];
